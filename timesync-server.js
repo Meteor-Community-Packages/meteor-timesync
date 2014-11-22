501383c85efaf74bab0b9f9668747a4b879ad29a
Vincent Jaubert <contact@vjaubert.com>
2014-11-22 13:20:27 +0100

    Added "text/plain" Mime type to fix Firefox syntax error reporting

--- timesync-server.js
+++ timesync-server.js
@@ -8,6 +8,7 @@ WebApp.rawConnectHandlers.use("/_timesync",
     res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", 0);
+    res.setHeader("Content-Type","text/plain");
 
     res.end(Date.now().toString());
   }
