3789e5bac97271fbbd4b372695cc4ab243a36c76
Andre Savic <andresavic@icloud.com>
2021-01-11 22:45:46 +0100

    Add capacitorjs support

--- timesync-server.js
+++ timesync-server.js
@@ -18,8 +18,9 @@ WebApp.rawConnectHandlers.use("/_timesync",
     // and http://meteor.local for earlier versions
     const origin = req.headers.origin;
 
-    if (origin && ( origin === 'http://meteor.local' || 
-        origin === 'meteor://desktop' || 
+    if (origin && ( origin === 'http://meteor.local' ||
+        origin === 'capacitor://meteor.local' ||
+        origin === 'meteor://desktop' ||
         /^http:\/\/localhost:1[23]\d\d\d$/.test(origin) ) ) {
       res.setHeader('Access-Control-Allow-Origin', origin);
     }
